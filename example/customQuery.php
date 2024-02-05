f0dddc82aea6910cfa9109b61dfbdacb7900bcc2
hywax <me@hywax.space>
2024-02-05 21:43:24 +0500

    docs(example): report service custom query

---
+++ example/customQuery.php
@@ -0,0 +1,27 @@
+<?php
+
+include '../vendor/autoload.php';
+
+use Hywax\YaMetrika\Exception\ClientException;
+use Hywax\YaMetrika\Service\ReportService;
+use Hywax\YaMetrika\Transformer\ReportDataTransformer;
+
+try {
+    $reportService = new ReportService([
+        'token' => getenv('TOKEN'),
+        'resultTransformer' => new ReportDataTransformer(),
+    ]);
+
+    $visitsWithoutRobots = $reportService->getCustomQuery([
+        'ids' => getenv('COUNTER'),
+        'date1' => '2024-01-01',
+        'date2' => '2024-01-31',
+        'metrics' => 'ym:s:visits',
+        'dimensions' => 'ym:s:searchEngine',
+        'filters' => "ym:s:trafficSource=='organic' AND ym:s:isRobot=='No'",
+    ]);
+
+    print_r($visitsWithoutRobots);
+} catch (ClientException $e) {
+    echo $e->getMessage();
+}
