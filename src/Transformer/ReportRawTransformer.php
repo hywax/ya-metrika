c1a2f81eff345650157c15229ee9cf991e7a2633
hywax <me@hywax.space>
2024-02-06 15:13:56 +0500

    docs: report raw transformer methods phpdoc

--- src/Transformer/ReportRawTransformer.php
+++ src/Transformer/ReportRawTransformer.php
@@ -6,6 +6,12 @@ use Hywax\YaMetrika\Interface\Transformer;
 
 class ReportRawTransformer implements Transformer
 {
+    /**
+     * Transform the data
+     *
+     * @param mixed $data
+     * @return array<mixed>
+     */
     public function transform($data): array
     {
         return $data;
