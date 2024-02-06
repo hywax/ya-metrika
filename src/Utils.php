3aebcd2003e0936d1d6314bee5f59e0585dcfaa0
hywax <me@hywax.space>
2024-02-06 15:13:23 +0500

    docs: units methods phpdoc

--- src/Utils.php
+++ src/Utils.php
@@ -2,12 +2,21 @@
 
 namespace Hywax\YaMetrika;
 
+use DateInterval;
+use DateTime;
+
 class Utils
 {
+    /**
+     * Get difference date
+     *
+     * @param int $days
+     * @return array<DateTime>
+     */
     public static function getDifferenceDate(int $days): array
     {
-        $endDate = new \DateTime();
-        $startDate = (clone $endDate)->sub(new \DateInterval("P{$days}D"));
+        $endDate = new DateTime();
+        $startDate = (clone $endDate)->sub(new DateInterval("P{$days}D"));
 
         return [$startDate, $endDate];
     }
