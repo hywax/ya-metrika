8072436790b5a8656a362a6fd4afdd2e073263ee
hywax <me@hywax.space>
2024-02-06 15:13:32 +0500

    docs: transformer methods phpdoc

--- src/Interface/Transformer.php
+++ src/Interface/Transformer.php
@@ -4,5 +4,11 @@ namespace Hywax\YaMetrika\Interface;
 
 interface Transformer
 {
+    /**
+     * Transform the data
+     *
+     * @param mixed $data
+     * @return mixed
+     */
     public function transform($data);
 }
